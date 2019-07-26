c5e29899a31f5958cd3de70533a25aeb48116453
tannaurus <contacttannergill@gmail.com>
2019-07-25 20:56:55 -0700

    Implemented pick up items

---
+++ Assets/Scripts/GrabbedItem.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabbedItem : MonoBehaviour
+{
+    public Collider player;
+
+    public void Init()
+    {
+        Physics.IgnoreCollision(player, GetComponent<Collider>(), true);
+    }
+
+    public void Reset() 
+    {
+        Physics.IgnoreCollision(player, GetComponent<Collider>(), false);
+    }
+}
--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -4,35 +4,49 @@ using UnityEngine;
 
 public class Inventory : MonoBehaviour
 {
-    public Transform reachHand;
-    public Transform hand;
-    public Transform head;
+    public Transform reach;
+    public Transform grab;
+    public Collider hand;
+
     public int reachDistance = 5;
     public int throwForce = 3;
 
-    private bool canHold = true;
-    private GameObject inHold;
+    private bool canReach = true;
+    private bool canGrab = true;
+    private GameObject inReach;
+    private GameObject inGrab;
 
     // Update is called once per frame
     void Update()
     {
         InputListener();
-        UpdateHoldItem();
+        UpdateItemsPos();
     }
 
     void InputListener()
     {
-        if (Input.GetKeyDown(KeyCode.F))
+        if (Input.GetKeyDown(KeyCode.R))
         {
-            OnHold();
+            OnReach();
+        }
+        if (Input.GetKeyDown(KeyCode.F)) 
+        {
+            OnGrab();
         }
     }
 
-    void UpdateHoldItem()
+    void UpdateItemsPos()
     {
-        if (canHold) return;
+        if (!canReach)
+        {
+            inReach.transform.position = Vector3.Lerp(inReach.transform.position, reach.position, 0.5f);
+        }
+        if (!canGrab) 
+        {
+            inGrab.transform.position = Vector3.Lerp(inGrab.transform.position, grab.position, 0.5f);
+            inGrab.transform.rotation = Quaternion.Lerp(inGrab.transform.rotation, grab.transform.rotation * Quaternion.identity, 10 * Time.deltaTime);
+        }
 
-        inHold.transform.position = Vector3.Lerp(inHold.transform.position, hand.position, 0.5f);
     }
 
     bool IsItem(Collider col)
@@ -40,40 +54,86 @@ public class Inventory : MonoBehaviour
         return col.gameObject.tag == "Item";
     }
 
-    void OnHold() 
+    void OnGrab()
     {
         RaycastHit hit;
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-        if (!canHold) {
-            Drop();
+        if (!canGrab)
+        {
+            DropGrab();
         }
         else if (Physics.Raycast(ray, out hit, reachDistance))
         {
             if (IsItem(hit.collider))
             {
-                Hold(hit.collider);
+                Grab(hit.collider);
             }
         }
     }
 
-    void Hold(Collider col)
+    void Grab(Collider col)
     {
-        inHold = col.gameObject;
-        inHold.transform.parent = hand;
-        Rigidbody holdRb = inHold.GetComponent<Rigidbody>();
-        holdRb.useGravity = false;
-        holdRb.constraints = RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionZ;
-        canHold = false;
+        inGrab = col.gameObject;
+        inGrab.transform.parent = grab;
+        GrabbedItem itemScript = inGrab.AddComponent<GrabbedItem>();
+        itemScript.player = hand;
+        itemScript.Init();
+        Rigidbody grabRb = inGrab.GetComponent<Rigidbody>();
+        grabRb.useGravity = false;
+        grabRb.constraints = RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
+        canGrab = false;
     }
 
-    void Drop()
+
+    void OnReach() 
     {
-        inHold.transform.parent = null;
-        Rigidbody holdRb = inHold.GetComponent<Rigidbody>();
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (!canReach) {
+            DropReach();
+        }
+        else if (Physics.Raycast(ray, out hit, reachDistance))
+        {
+            if (IsItem(hit.collider))
+            {
+                Reach(hit.collider);
+            }
+        }
+    }
+
+    void Reach(Collider col)
+    {
+        inReach = col.gameObject;
+        inReach.transform.parent = reach;
+        Rigidbody holdRb = inReach.GetComponent<Rigidbody>();
+        holdRb.useGravity = false;
+        holdRb.constraints = RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionZ;
+        canReach = false;
+    }
+
+    void DropReach()
+    {
+        inReach.transform.parent = null;
+        Rigidbody holdRb = inReach.GetComponent<Rigidbody>();
         holdRb.useGravity = true;
         holdRb.constraints = RigidbodyConstraints.FreezePositionY & RigidbodyConstraints.FreezePositionX & RigidbodyConstraints.FreezePositionZ;
         holdRb.AddForce(transform.forward * throwForce);
-        canHold = true;
+        canReach = true;
+        inReach = null;
+    }
+
+    void DropGrab()
+    {
+        inGrab.transform.parent = null;
+        Rigidbody grabRb = inGrab.GetComponent<Rigidbody>();
+        grabRb.useGravity = true;
+        grabRb.constraints = RigidbodyConstraints.FreezePositionY & RigidbodyConstraints.FreezePositionX & RigidbodyConstraints.FreezePositionZ;
+        grabRb.AddForce(transform.forward * throwForce);
+        GrabbedItem itemScript = inGrab.GetComponent<GrabbedItem>();
+        itemScript.Reset();
+        Destroy(itemScript);
+        canGrab = true;
+        inGrab = null;
     }
 }
