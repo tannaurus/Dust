5f0ed73ac304b69575acf836f0c72ca7b3d36500
tannaurus <contacttannergill@gmail.com>
2019-12-30 17:41:34 -0800

    Player movement, animations, basic terrain playground.

---
+++ Assets/Scripts/Player/Player_Controller.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Controller : MonoBehaviour
+{
+		public float speedMultiplier = 1f; 
+		public float rotationMultiplier = 10f;
+		public float jumpForce = 5f;
+    private Animator animator;
+		private Rigidbody rigidbody;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+				rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+			// Rotate player
+			if (Input.GetKey(KeyCode.A)) {
+				transform.Rotate(-Vector3.up * rotationMultiplier * Time.deltaTime);
+			}
+			if (Input.GetKey(KeyCode.D)) {
+				transform.Rotate(Vector3.up * rotationMultiplier * Time.deltaTime);
+			}
+			
+			// Move player
+			Vector3 displacement = transform.position;
+			if (Input.GetKey(KeyCode.W) | Input.GetKey(KeyCode.S)) {
+				animator.SetTrigger("Walk");
+				if (Input.GetKey(KeyCode.W)) {
+					displacement += transform.forward;
+				}
+				if (Input.GetKey(KeyCode.S)) {
+					displacement -= transform.forward;
+				}
+			} else {
+				animator.SetTrigger("Idle");
+			}
+			transform.position = Vector3.Lerp(transform.position, displacement, speedMultiplier * Time.deltaTime);
+
+			// Jump
+			if (IsGrounded() & Input.GetKeyDown(KeyCode.Space)) {
+				rigidbody.AddForce(transform.up * jumpForce);
+			}
+    }
+
+
+		bool IsGrounded() {
+			return Physics.Raycast(transform.position, -Vector3.up, 0.5f);
+		}
+}
