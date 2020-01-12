2bfd29bed41334d1dd7b17148fca11430f82a0b1
tannaurus <contacttannergill@gmail.com>
2020-01-12 13:13:37 -0800

    Updated Item_Float to monitor last known item location and init float if that changed to the None state

--- Assets/Scripts/Items/Item_Float.cs
+++ Assets/Scripts/Items/Item_Float.cs
@@ -9,33 +9,56 @@ public class Item_Float : MonoBehaviour
 	public float floatSpeed = 1.2f;
 	private Item item;
 	private float holdHeight;
+	private ItemLocation lastKnownLocation;
 
 	void Start()
 	{
 		Init();
+		if (item.location == ItemLocation.None) {
+			InitFloat();
+		}
 	}
 
 	void Update()
 	{
-		Float();
+		if (
+			lastKnownLocation != ItemLocation.None &&
+			item.location == ItemLocation.None
+		) {
+			InitFloat();
+		}
+		if (item.location == ItemLocation.None) {
+			Float();
+		}
+
+		// Always do last.
+		UpdateLastKnownLocation();
 	}
 
 	// Updaters
 	void Init() {
 		item = GetComponent<Item>();
+		lastKnownLocation = item.location;
+	}
+
+	void InitFloat() {
 		Vector3 itemPos = transform.position;
 		itemPos.y = Terrain.activeTerrain.SampleHeight(transform.position);
-		Debug.Log(itemPos.y);
 		itemPos.y += floatHeight;
 		// Keep track of this offset for the float logic
 		holdHeight = itemPos.y;
-		Debug.Log(itemPos.y);
 		transform.position = itemPos;
 	}
-
+	
 	void Float() {
 		Vector3 pos = transform.position;
 		float newY = (Mathf.Sin(Time.time * floatSpeed) + holdHeight) * floatOffset;
 		transform.position = new Vector3(pos.x, newY, pos.z);
+	}
+
+	void UpdateLastKnownLocation() {
+		if (lastKnownLocation != item.location) {
+			lastKnownLocation = item.location;
+		}
 	}
 }
